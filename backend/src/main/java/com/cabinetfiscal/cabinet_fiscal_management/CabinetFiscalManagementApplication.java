40a52e785be788cb8a7b37a71dbe90e8e049bf81
Yasmine Cherni <yassmine05cherni@gmail.com>
2026-03-07 01:49:53 +0100

    Initial project structure

---
+++ backend/src/main/java/com/cabinetfiscal/cabinet_fiscal_management/CabinetFiscalManagementApplication.java
@@ -0,0 +1,13 @@
+package com.cabinetfiscal.cabinet_fiscal_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinetFiscalManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabinetFiscalManagementApplication.class, args);
+	}
+
+}
